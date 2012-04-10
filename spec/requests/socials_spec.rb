f3e4c339c2e48188ac704140e0c99543f75cb5a9
Ryan Rix <ry@n.rix.si>
2012-04-09 21:01:30 -0700

    Add analytics

--- spec/requests/socials_spec.rb
+++ spec/requests/socials_spec.rb
@@ -7,9 +7,26 @@ describe "Socials" do
       visit root_path
     end
 
-    it 'generates a social+ cloud' do
-      page.should have_content "Here's your Cloud name, time for the IPO!"
-      page.should have_css     "h1"
+    context "new user" do
+
+      it 'generates a social+ cloud' do
+        page.should have_content "Here's your Cloud name, time for the IPO!"
+        page.should have_css     "h1"
+      end
+
+    end
+
+    context "returning cloud+'er" do
+
+      before do
+        @ccloud = Cloud.create_new_cloud
+        visit cloud_path @ccloud
+      end
+
+      it 'users can find old cloud++s again.' do
+        page.should have_content @ccloud.cloud_name
+      end
+
     end
 
   end
