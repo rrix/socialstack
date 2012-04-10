9dc6cf663c5bb92f55706ee425c65058f924dc92
Ryan Rix <ry@n.rix.si>
2012-04-09 19:20:49 -0700

    Begin speccing the thigns

---
+++ spec/requests/socials_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Socials" do
+  describe "As a guest ISBAT social" do
+
+    before do
+      visit root_path
+    end
+
+    it 'generates a social+ name' do
+      page.should have_content "Here's your Cloud name, time for the IPO!"
+      page.should have_css     "h1"
+    end
+
+  end
+end
